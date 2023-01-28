69eac18f56473bdae6496ae94e002f02ea47371a
Timileyin Adeleke <101255913+timo-shey@users.noreply.github.com>
2023-01-28 21:54:51 +0100

    chore: api gateway

--- employee-service/src/main/java/com/timoshey/employeeservice/service/ApiClient.java
+++ employee-service/src/main/java/com/timoshey/employeeservice/service/ApiClient.java
@@ -2,11 +2,10 @@ package com.timoshey.employeeservice.service;
 
 import com.timoshey.employeeservice.dto.DepartmentDto;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "http://localhost:8080", value = "DEPARTMENT-SERVICE")
+@FeignClient(name = "DEPARTMENT-SERVICE")
 public interface ApiClient {
     @GetMapping("api/departments/{department-code}")
     DepartmentDto getDepartment(@PathVariable("department-code") String departmentCode);
