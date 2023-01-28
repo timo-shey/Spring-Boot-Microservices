9db7716eb77f101e45007a4a80ff70e67329958c
Timileyin Adeleke <101255913+timo-shey@users.noreply.github.com>
2023-01-28 11:32:27 +0100

    chore: registered services as Eureka Client

--- employee-service/src/main/java/com/timoshey/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/com/timoshey/employeeservice/EmployeeServiceApplication.java
@@ -2,6 +2,7 @@ package com.timoshey.employeeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -9,6 +10,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableEurekaClient
 public class EmployeeServiceApplication {
 
 //	@Bean
