e85aa7a47c15f354111a1b7e6f4257d7c2d7b10d
Timileyin Adeleke <101255913+timo-shey@users.noreply.github.com>
2023-01-31 10:36:42 +0100

    chore: Circuit Breaker - Resilience4J

--- employee-service/src/main/java/com/timoshey/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/com/timoshey/employeeservice/EmployeeServiceApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -15,10 +17,10 @@ public class EmployeeServiceApplication {
 //		return new RestTemplate();
 //	}
 
-//	@Bean
-//	public WebClient webClient() {
-//		return WebClient.builder().build();
-//	}
+	@Bean
+	public WebClient webClient() {
+		return WebClient.builder().build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeServiceApplication.class, args);
--- employee-service/src/main/java/com/timoshey/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/com/timoshey/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -8,17 +8,24 @@ import com.timoshey.employeeservice.exception.EmployeeNotFoundException;
 import com.timoshey.employeeservice.repository.EmployeeRepository;
 import com.timoshey.employeeservice.service.ApiClient;
 import com.timoshey.employeeservice.service.EmployeeService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 @AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeServiceImpl.class);
+
     private EmployeeRepository employeeRepository;
 //    private RestTemplate restTemplate;
-//    private WebClient webClient;
+    private WebClient webClient;
     private ApiClient apiClient;
     @Override
     public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
@@ -26,9 +33,13 @@ public class EmployeeServiceImpl implements EmployeeService {
         Employee savedEmployee = employeeRepository.save(employee);
         return EmployeeDto.mapToEmployeeDto(savedEmployee);
     }
-
+//    @CircuitBreaker(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartment")
+    @Retry(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartment")
     @Override
     public ApiResponseDto getEmployeeById(Long employeeId) {
+
+        LOGGER.info("inside getEmployeeById() method");
+
         Employee employee = employeeRepository.findById(employeeId).orElseThrow(
                 ()-> new EmployeeNotFoundException(HttpStatus.NOT_FOUND, "Employee Does Not Exist")
         );
@@ -38,13 +49,35 @@ public class EmployeeServiceImpl implements EmployeeService {
 //
 //        DepartmentDto departmentDto = responseEntity.getBody();
 
-//        DepartmentDto departmentDto = webClient.get()
-//                .uri("http://localhost:8080/api/departments/" + employee.getDepartmentCode())
-//                .retrieve()
-//                .bodyToMono(DepartmentDto.class)
-//                .block();
+        DepartmentDto departmentDto = webClient.get()
+                .uri("http://localhost:8080/api/departments/" + employee.getDepartmentCode())
+                .retrieve()
+                .bodyToMono(DepartmentDto.class)
+                .block();
 
-        DepartmentDto departmentDto = apiClient.getDepartment(employee.getDepartmentCode());
+//        DepartmentDto departmentDto = apiClient.getDepartment(employee.getDepartmentCode());
+
+        EmployeeDto employeeDto = EmployeeDto.mapToEmployeeDto(employee);
+
+        ApiResponseDto apiResponseDto = new ApiResponseDto();
+        apiResponseDto.setEmployeeDto(employeeDto);
+        apiResponseDto.setDepartmentDto(departmentDto);
+
+        return apiResponseDto;
+    }
+
+    public ApiResponseDto getDefaultDepartment(Long employeeId, Exception exception) {
+
+        LOGGER.info("inside getDefaultDepartment() method");
+
+        Employee employee = employeeRepository.findById(employeeId).orElseThrow(
+                ()-> new EmployeeNotFoundException(HttpStatus.NOT_FOUND, "Employee Does Not Exist")
+        );
+
+        DepartmentDto departmentDto = new DepartmentDto();
+        departmentDto.setDepartmentName("R&D Department");
+        departmentDto.setDepartmentCode("RD001");
+        departmentDto.setDepartmentDescription("Research and Development Department");
 
         EmployeeDto employeeDto = EmployeeDto.mapToEmployeeDto(employee);
 
